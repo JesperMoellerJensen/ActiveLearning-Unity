2e4bafba2c11af46da5d7992aeccfcf77fb23f24
Jesper <jesp626a@edu.eal.dk>
2019-04-15 16:20:29 +0200

    Added timer to Landmark

--- Assets/Scripts/Landmark.cs
+++ Assets/Scripts/Landmark.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
 public class Landmark : MonoBehaviour
 {
 	public float Radius = 3f;
+	public float Cooldown = 15f;
+	public float CurrentCooldown;
+
+	public Resource LandmarkResource;
 
 	private List<Group> ActiveGroups = new List<Group>();
 
@@ -13,6 +19,37 @@ public class Landmark : MonoBehaviour
 	private void Awake()
 	{
 		_triggerArea = GetComponent<CircleCollider2D>();
+		InitializeLandmark();
+	}
+
+	private void InitializeLandmark()
+	{
+		LandmarkResource = (Resource)Random.Range(0, System.Enum.GetValues(typeof(Resource)).Length);
+		Debug.Log(LandmarkResource.ToString());
+	}
+
+	private void GroupOnPoint(Group group)
+	{
+		Debug.Log(string.Format("Everyone from group {0} is here", group.GroupId));
+		ActiveGroups.Add(group);
+
+		//TODO: Implement Questions
+		ActiveGroups.Remove(group);
+		Debug.Log("You got 1 " + LandmarkResource.ToString());
+		OnCooldown();
+	}
+
+	private void GroupNotOnPoint(List<Player> players)
+	{
+		foreach (Player player in players)
+		{
+			Debug.Log(string.Format("Player {0} is missing", player.Name));
+		}
+	}
+
+	private void GroupMemberLeavingActiveArea(Group group, Player player)
+	{
+		Debug.Log(string.Format("Player {0} from group {1} has left the area, please return", player.Name, group.GroupId));
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -42,30 +79,34 @@ public class Landmark : MonoBehaviour
 		}
 	}
 
-	private void GroupOnPoint(Group group)
+	private void OnCooldown()
 	{
-		Debug.Log(string.Format("Everyone from group {0} is here", group.GroupId));
-		ActiveGroups.Add(group);
-
-		//TODO: Implement Questions
+		CurrentCooldown = Cooldown;
+		InvokeRepeating("UpdateCooldown", 1, 1);
+		Debug.Log("Landmark going on cooldown");
 	}
 
-	private void GroupNotOnPoint(List<Player> players)
+	private void UpdateCooldown()
 	{
-		foreach (Player player in players)
-		{
-			Debug.Log(string.Format("Player {0} is missing", player.Name));
-		}
+		CurrentCooldown--;
+		UpdateTimer();
+		Debug.Log("Cooldown left: " + CurrentCooldown);
+		if (CurrentCooldown <= 0)
+			OffCooldown();
+	}
+	private void OffCooldown()
+	{
+		CancelInvoke("UpdateCooldown");
+		Debug.Log("Landmark not longer on cooldown");
+
 	}
 
-	private void GroupMemberLeavingActiveArea(Group group, Player player)
+	private void UpdateTimer()
 	{
-		Debug.Log(string.Format("Player {0} from group {1} has left the area, please return", player.Name, group.GroupId));
-	}
-
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.blue;
-		Gizmos.DrawWireSphere(transform.position, Radius);
+		MeshRenderer TimerRendere = GetComponentInChildren<MeshRenderer>();
+		TextMeshPro textMeshPro = GetComponentInChildren<TextMeshPro>();
+		textMeshPro.text = CurrentCooldown.ToString();
+		TimerRendere.material.SetFloat("_Cutoff", (1 - CurrentCooldown / Cooldown));
+		Debug.Log("asjdhakjhdkajshd " + (1 - CurrentCooldown / Cooldown));
 	}
 }
